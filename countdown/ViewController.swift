8c1f5a0c9432fe550a077154a88c17940b37471c
Don Browning <don.browning@gmail.com>
2021-01-15 13:48:46 -0500

    Initial Commit

---
+++ countdown/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  countdown
+//
+//  Created by Don  Browning on 1/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
